90b37bb5932fc44f746f6c824a072829c938e871
airconditioner75 <leongjingshan@gmail.com>
2025-08-15 14:12:54 +0800

    Minor Bug Fixes
    Fixes to the collision, prevent the objects from falling out of the floor/map, by making their collision detection to continuous.

--- Assets/GADV_Project/Scripts/PlayerMechanics/ItemDragManager.cs
+++ Assets/GADV_Project/Scripts/PlayerMechanics/ItemDragManager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Presets;
 using UnityEngine;
 
 public class ItemDragManager : MonoBehaviour
